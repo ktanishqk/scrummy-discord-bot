a8cddbac28395cf5f0029e32bb17e6544bddd5de
ktanishqk <68249492+ktanishqk@users.noreply.github.com>
2021-06-07 11:59:30 -0500

    More support for project object in bot  and major bug fixes

--- bot.py
+++ bot.py
@@ -1,53 +1,83 @@
-import project_handling
-import discord
+from project_handling import Project
 import discord.ext
 from discord.ext import commands
+from discord.ext.commands import MemberConverter
+import asyncio
+from enum import Enum
 
 # Initializing the variables required for the bot
 
-#current_project = Project()
 projects = []
 bot = commands.Bot(command_prefix='!')
-client = discord.Client()
-start = "Hey, I am here. What's up?"
-new_project_info= "Alright, let's start a new project. \nGo ahead and give the new project a name!"
-
 
 @bot.command(name="start")
-async def start(ctx):
+async def start_bot(ctx):
     """ Start function that uses the !start command to begin the bot within the server. """
-    await ctx.send(start)
+    await ctx.send("Hey, I am here. What's up?")
 
 @bot.command(name="new")
-async def new(ctx, *args):
+async def new_project(ctx):
     """ New Project function intializes a new project within the server. """
-    await ctx.send(new_project_info)
+    await ctx.send("Alright, let's start a new project. \nGo ahead and give the new project a name!")
+    new_project_name = await input(ctx)
+    if new_project_name:
+        await ctx.send("New Project Created: " + new_project_name.content)
+        #await asyncio.sleep(3)
+        await ctx.send("The project needs a manager, doesn't it? Appoint one!")
+        project_manager = await input(ctx)
+        if project_manager:
+            await ctx.send(project_manager.content + " has been appointed as the project manager for " + new_project_name.content)   
+            #await asyncio.sleep(3)
+            await ctx.send("The project needs some members. Just tag the members you would like to add!")
+            members = await input(ctx)
+            if members:
+                await ctx.send(members.content + " has been appointed as the members for the project: " + new_project_name.content)   
+                #await asyncio.sleep(3)
+                await ctx.send("Finally, let's attach a priority tag to the project! Choose from high, medium, or low.")
+                priority_tag = await input(ctx)
+                if priority_tag:
+                    await ctx.send(new_project_name.content + "project has been appointed " + (str(priority_tag.content)).upper() + " priority tag.")   
+                    #await asyncio.sleep(3)
+                    await ctx.send("The project has been created!")
+            
+
+    new_project = Project(name = new_project_name.content, project_manager= project_manager.content, members = members.content, priority_tag=str(priority_tag).upper())
+    projects.append(new_project)
+@bot.command(name = "project_list")
+async def get_project_list(ctx):
+    """ Function to get the project list for a particular server"""
+
+    embed=discord.Embed(title="Project List", description = "Here's the list of projects going on:")
+    for i in range(len(projects)):
+        temp_project = projects[i]
+        embed.add_field(name = str(temp_project.get_name), value= str(temp_project.get_project_manager), inline= True)
+    await ctx.send(embed=embed)
+
+# @bot.command(name = "add_manager")
+# async def add_project_manager(ctx):
+#     """Function that adds a manager to an already existing project"""
+#     await ctx.send("Please choose a project from the following list to add a manager to:")
+#     get_project_list(ctx)
+#     try:
+#         project = await bot.wait_for("message", timeout=60, 
+#                         check= lambda message : message.author == ctx.author 
+#                         and message.channel == ctx.channel #lambda function 
+#                                 #to make sure that the message received is by the author within the same channel
+#         )
+
+async def input(ctx):
     try:
-        new_project_name = await bot.wait_for(
-            "message", 
-            timeout= 60, 
-            check = lambda message : message.author == ctx.author 
-                            and message.channel == ctx.channel
-            )
-        if new_project_name:
-            await ctx.send("New Project Created: " + str(new_project_name.content))
+        input_message = await bot.wait_for(
+                "message", 
+                timeout= 60, #the timeout period for the input
+                check = lambda message : message.author == ctx.author 
+                                and message.channel == ctx.channel #lambda function to make sure that the message received is by the author within the same channel
+                )
+        return input_message 
     except TimeoutError:
-        await ctx.send("You took too much time, try again!")  
+        await ctx.send("Looks like you took too much time to respond. Try again!")
 
 with open("BOT_TOKEN.txt", "r") as token_file:
     TOKEN = token_file.read()
     print("Token file read")
     bot.run(TOKEN)
-
-# @client.event
-# async def on_message(message):
-#     """ Function to handle the messages within the channel """
-#     if message.author == client.user:
-#         # if message.content == new_project:
-#             # else:
-#         return    
-#    #if message.
-#
-# @bot.command(name = "current")
-# async def current_project(ctx):
-#     await ctx.send(projects[-1])
--- project_handling.py
+++ project_handling.py
@@ -1,3 +1,24 @@
 from discord.ext import commands
 import discord
 import discord.ext
+
+class Project:
+    def __init__(self, name, project_manager, members, priority_tag):
+        self.name = name
+        self.project_manager = project_manager
+        self.members = members
+        self.priority_tag = priority_tag    
+    def get_name(self):
+        return self.name
+    def set_project_manager(self, project_manager):
+        self.project_manager = project_manager
+    def get_project_manager(self):
+        return self.project_manager
+    def set_members(self, members):
+        self.members = members
+    def get_members(self):
+        return self.members
+    def set_priority_tag(self, priority_tag):
+        self.priority_tag = priority_tag
+    def get_priority_tag(self):
+        return self.priority_tag
